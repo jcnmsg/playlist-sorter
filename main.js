9f7500953f114350de96740b5c4938c9527fc685
Johnny <19619630+jcnmsg@users.noreply.github.com>
2023-10-08 16:52:07 +0100

    Remove unneeded step logic, everything done synchronously

--- main.js
+++ main.js
@@ -2,93 +2,74 @@ import fs, { existsSync } from 'fs';
 import path from 'path';
 import yargs from 'yargs'
 import { hideBin } from 'yargs/helpers';
-import { Pipeline } from './lib/pipeline.js'
 
 const args = ['songs', 'playlist', 'output'];
 const argv = yargs(hideBin(process.argv)).argv;
-const pipeline = new Pipeline();
+const outputDir = path.join(argv.output, path.basename(argv.playlist, '.csv'));
+const playlist = fs.readFileSync(argv.playlist, 'utf8');
+const songs = playlist
+  .split('\n')
+  .map((line) => {
+    let [, , title, album, artist] = line ? line.match(/(".*?"|[^",\s]+)(?=\s*,|\s*$)/g) : [];
 
-pipeline.step((data, next) => {
-  args.forEach((arg) => {
-    if (!argv[arg]) {
-      throw new Error(`Missing argument: ${arg}`);
+    title = title ? title.replace(/"/g, '') : '';
+    album = album ? album.replace(/"/g, '') : '';
+    artist = artist ? artist.replace(/"/g, '').split(',')[0] : '';
+
+    if (artist && album && title) {
+      return { artist, album, title };
     }
+  })
+  .filter((song, index) => index > 0 && song);
 
-    if (arg === 'songs' && !existsSync(argv[arg])) {
-      throw new Error(`Song directory does not exist: ${argv[arg]}`);
-    }
-
-    if (arg === 'playlist' && !existsSync(argv[arg]) && path.extname(argv[arg]) !== '.csv') {
-      throw new Error(`Playlist file does not exist or is not a csv: ${argv[arg]}`);
-    }
-
-    if (arg === 'output' && !existsSync(argv[arg])) {
-      throw new Error(`Output directory does not exist: ${argv[arg]}`);
-    }
-  });
-
-  next();
-});
-
-pipeline.step((data, next) => {
-  const outputDir = path.join(argv.output, path.basename(argv.playlist, '.csv'));
-
-  if (!existsSync(outputDir)) {
-    fs.mkdirSync(outputDir);
+// Validate args
+args.forEach((arg) => {
+  if (!argv[arg]) {
+    throw new Error(`Missing argument: ${arg}`);
   }
 
-  next();
+  if (arg === 'songs' && !existsSync(argv[arg])) {
+    throw new Error(`Song directory does not exist: ${argv[arg]}`);
+  }
+
+  if (arg === 'playlist' && !existsSync(argv[arg]) && path.extname(argv[arg]) !== '.csv') {
+    throw new Error(`Playlist file does not exist or is not a csv: ${argv[arg]}`);
+  }
+
+  if (arg === 'output' && !existsSync(argv[arg])) {
+    throw new Error(`Output directory does not exist: ${argv[arg]}`);
+  }
 });
 
-pipeline.step((data, next) => {
-  const playlist = fs.readFileSync(argv.playlist, 'utf8');
+// Create output dir
+if (!existsSync(outputDir)) {
+  fs.mkdirSync(outputDir);
+}
 
-  data.songs = playlist
-    .split('\n')
-    .map((line) => {
-      let [,, title, album, artist] = line ? line.match(/(".*?"|[^",\s]+)(?=\s*,|\s*$)/g) : [];
+let copied = 0;
 
-      title = title ? title.replace(/"/g, '') : '';
-      album = album ? album.replace(/"/g, '') : '';
-      artist = artist ? artist.replace(/"/g, '').split(',')[0] : '';
+// Sort the songs into playlist
+songs.forEach((song) => {
+  const songPath = path.join(argv.songs, song.artist, song.album);
 
-      if (artist && album && title) {
-        return { artist, album, title };
-      }
-    })
-    .filter((song, index) => index > 0 && song);
+  try {
+    const files = fs.readdirSync(songPath);
+    const songFile = files.find((file) => file.includes(song.title) && file.endsWith('.ogg'));
+    const destPath = path.join(argv.output, path.basename(argv.playlist, '.csv'), `${song.artist} - ${song.title}.ogg`);
 
-    next();
-});
+    copied++;
 
-pipeline.step((data, next) => {
-  data.copied = 0;
-  data.songs.forEach((song) => {
-    const songPath = path.join(argv.songs, song.artist, song.album);
-
-    try {
-      const files = fs.readdirSync(songPath);
-      const songFile = files.find((file) => file.includes(song.title) && file.endsWith('.ogg'));
-      const destPath = path.join(argv.output, path.basename(argv.playlist, '.csv'), `${song.artist} - ${song.title}.ogg`);
-
-      data.copied++;
-      
-      if (!existsSync(destPath) && existsSync(songPath)) {
-        fs.copyFileSync(path.join(songPath, songFile), destPath);
-      }
+    if (!existsSync(destPath) && existsSync(songPath)) {
+      fs.copyFileSync(path.join(songPath, songFile), destPath);
     }
-    catch(err) {
-      const destPath = path.join(argv.output, path.basename(argv.playlist, '.csv'), `${song.artist} - ${song.title}.ogg`);
+  }
+  catch (err) {
+    const destPath = path.join(argv.output, path.basename(argv.playlist, '.csv'), `${song.artist} - ${song.title}.ogg`);
 
-      if (!existsSync(destPath)) { // Check if file got copied manually and log if missing (it can still error out if the file includes ":" or "/" on Windows)
-        console.log('Error: ', songPath)
-      }
+    if (!existsSync(destPath)) { // Check if file got copied manually and log if missing (it can still error out if the file includes ":" or "/" on Windows)
+      console.log('Error: ', songPath)
     }
-  });
-
-  console.log(`Finished and copied ${data.copied} songs on ${argv['playlist']}`)
-
-  next();
+  }
 });
 
-pipeline.run({});
+console.log(`Finished and copied ${copied} songs on ${argv['playlist']}`)
